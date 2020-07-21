34f5ab412c470d2825e5454a1065a90de83f0cdd
Anthony Matteis <amatt9425@gmail.com>
2020-07-21 15:29:00 -0500

    End of Section 15

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -22,6 +22,7 @@ import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
 import { MemberListResolver } from './_resolvers/member-list.resolver';
 import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
 import { MemberEditResolver } from './_resolvers/member-edit.resolver';
+import { ListsResolver } from './_resolvers/lists.resolver';
 
 import { ErrorInterceptorProvider } from './_services/error.interceptor';
 import { AuthService } from './_services/auth.service';
@@ -85,7 +86,8 @@ export function tokenGetter(): string {
       MemberDetailResolver,
       MemberListResolver,
       MemberEditResolver,
-      PreventUnsavedChanges
+      PreventUnsavedChanges,
+      ListsResolver
    ],
    bootstrap: [
       AppComponent
--- DatingApp-SPA/src/app/lists/lists.component.ts
+++ DatingApp-SPA/src/app/lists/lists.component.ts
@@ -1,15 +1,48 @@
+import { AlertifyService } from './../_services/alertify.service';
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from 'src/app/_services/user.service';
+import { AuthService } from './../_services/auth.service';
+import { Pagination, PaginatedResult } from './../_models/pagination';
 import { Component, OnInit } from '@angular/core';
+import { User } from '../_models/user';
 
 @Component({
   selector: 'app-lists',
   templateUrl: './lists.component.html',
-  styleUrls: ['./lists.component.css']
+  styleUrls: ['./lists.component.css'],
 })
 export class ListsComponent implements OnInit {
+  users: User[];
+  pagination: Pagination;
+  likesParam: string;
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private userService: UserService,
+    private route: ActivatedRoute,
+    private alertify: AlertifyService
+  ) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.route.data.subscribe(data => {
+      this.users = data.users.result;
+      this.pagination = data.users.pagination;
+    });
+    this.likesParam = 'Likers';
   }
 
+  loadUsers(): void {
+    this.userService.getUsers(this.pagination.currentPage.toString(), this.pagination.itemsPerPage.toString(), null, this.likesParam)
+      .subscribe((res: PaginatedResult<User[]>) => {
+      this.users = res.result;
+      this.pagination = res.pagination;
+    }, error => {
+      this.alertify.error(error);
+    });
+  }
+
+  pageChanged(event: any): void {
+    this.pagination.currentPage = event.page;
+    this.loadUsers();
+  }
 }
